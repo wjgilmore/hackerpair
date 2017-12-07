8244208939d6f556e0135e9419e634243b2f9391
Jason Gilmore <wj@wjgilmore.com>
2017-12-07 13:22:12 -0500

    Added table header to home page and updated the HackerPair catchphrase

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -3,8 +3,10 @@
 @section('jumbotron')
     <div class="jumbotron">
         <div class="container">
-            <h1>HackerPair Helps You Build Stuff Faster.</h1>
-            <h2>Learn, teach, hack, and make friends with developers in your city.</h2>
+            <div>
+            <h1>Build Faster with HackerPair</h1>
+            <h2 class="d-none d-md-block">Learn, teach, hack, and make friends with developers in your city.</h2>
+            </div>
         </div>
     </div>
 @endsection
@@ -13,7 +15,7 @@
 
     <div class="row">
         <div class="col mh-100">
-
+            <h2>The Latest Events</h2>
             @include('partials/_events_table', ['events' => $events])
 
         </div>

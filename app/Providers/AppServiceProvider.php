ce668a196686a86526b1276fb959665908da057b
Jason Gilmore <wj@wjgilmore.com>
2017-12-18 15:25:07 -0500

    Removed Dusk-specific registration code due to auto-discovery

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
 
-        if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DuskServiceProvider::class);
-        }
-
     }
 }
